e0a990f52aaa2f7405d15a16c877cdf618a54e06
dany5639 <dany_woozie@hotmail.com>
2020-12-08 21:20:39 +0100

    Initial upload.

---
+++ SteamSorter/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SteamSorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column2,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(801, 452);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "AppID";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 70;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Size";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 70;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Title";
+            this.Column1.Name = "Column1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(667, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Steam game library path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "SteamSorter";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ SteamSorter/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteamSorter
+{
+    public partial class Form1 : Form
+    {
+        private string path = @""; // need to auto get paths
+        /// <summary>
+        /// A collection of the games found in appmanifest files.
+        /// </summary>
+        private Dictionary<int, item> items;
+        public class item
+        {
+            public string title;
+            public long size;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            items = new Dictionary<int, item>();
+
+            toolStripStatusLabel1.Text = "Double click on any cell to uninstall a game.";
+
+        }
+        /// <summary>
+        /// Read any text file and return as a list of strings, one string per line.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        private static List<string> ReadTextFile(string filename)
+        {
+            var output = new List<string>();
+
+            using (var reader = new StreamReader(filename))
+            {
+                var line = "";
+                while (line != null)
+                {
+                    line = reader.ReadLine();
+                    output.Add(line);
+                }
+
+                if (output.Last() == null)
+                    output.RemoveAt(output.Count - 1);
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Read every Steam appmanifest file to get the appid, game title, and game size on disk.
+        /// </summary>
+        private void ReadFiles()
+        {
+            var files = Directory.EnumerateFiles(path);
+
+            foreach (var file in files)
+            {
+                if (!file.EndsWith(".acf"))
+                    continue;
+
+                // get appid from filename
+                var appid_s = file.Split("_".ToCharArray());
+                var appid_s2 = appid_s[1].Split(".".ToCharArray())[0];
+                int.TryParse(appid_s2, System.Globalization.NumberStyles.Integer, null, out int appid);
+
+                var item1 = new item();
+                int collectedValues = 0;
+
+                var text = ReadTextFile(file);
+                foreach (var line in text)
+                {
+                    if (line.Contains("\"appid\""))
+                    {
+                        var line2 = line.Split("		".ToCharArray());
+                        var line3 = line2[3].Split("\"".ToCharArray());
+                        var line4 = line3[1];
+                        int.TryParse(line4, System.Globalization.NumberStyles.Integer, null, out int appid_check);
+                        if (appid_check != appid)
+                            continue;
+                            // throw new Exception("ERROR: filename appid missmatches the appid in the file.");
+
+                        collectedValues++;
+
+                    }
+
+                    if (line.Contains("\"name\""))
+                    {
+                        var line2 = line.Split("		".ToCharArray());
+                        var line3 = line2[3];
+                        item1.title = line3;
+
+                        collectedValues++;
+
+                    }
+
+                    if (line.Contains("\"SizeOnDisk\""))
+                    {
+                        var line2 = line.Split("		".ToCharArray());
+                        var line3 = line2[3];
+                        var line4 = line3.Substring(1, line3.Length - 2);
+                        long.TryParse(line4, System.Globalization.NumberStyles.Integer, null, out long size);
+                        item1.size = size;
+
+                        collectedValues++;
+
+                    }
+
+                }
+
+                if (collectedValues < 3)
+                    continue;
+
+                if (!items.ContainsKey(appid))
+                    items.Add(appid, item1);
+
+            }
+        }
+        /// <summary>
+        /// Fill up the table with the found games: appid, title, size.
+        /// </summary>
+        private void PopulateList()
+        {
+            dataGridView1.Rows.Clear();
+
+            int i = -1;
+            foreach (var a in items)
+            {
+                i++;
+                var size = $"{((a.Value.size / 1024 / 1024)):D6}";
+                size = $"{size.Substring(0, 3)},{size.Substring(3,3)} GB";
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = $"{a.Key:D8}";
+                dataGridView1.Rows[i].Cells[1].Value = size;
+                dataGridView1.Rows[i].Cells[2].Value = a.Value.title;
+
+            }
+        }
+        /// <summary>
+        /// Double click on any row to call Steam and uninstall the game.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count == 0)
+                return;
+
+            var rowindex = dataGridView1.SelectedCells[0].RowIndex;
+
+            var a = dataGridView1.Rows[rowindex].Cells[0].Value;
+
+            int.TryParse(a.ToString(), System.Globalization.NumberStyles.Integer, null, out int val);
+
+            var path = $"steam://uninstall/{val}";
+
+            Process.Start(path);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            path = folderBrowserDialog1.SelectedPath;
+
+            ReadFiles();
+
+            PopulateList();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
